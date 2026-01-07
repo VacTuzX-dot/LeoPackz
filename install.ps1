dab77ce32c27e5fe2872db683245e90bf7c99802
VacTuzX-dot <lilitxrie@gmail.com>
2026-01-07 11:08:33 +0700

    chore: Change install directory to Downloads

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 
 # Configuration
 $AppName = "LeoPack"
-$InstallDir = "$env:LOCALAPPDATA\$AppName"
+$InstallDir = "$env:USERPROFILE\Downloads\$AppName"
 # IMPORTANT: Replace this URL with the direct link to your LeoPacks.zip file
 # Example: "https://github.com/YourUser/YourRepo/releases/download/v1.0/LeoPacks.zip"
 $ZipUrl = "https://github.com/VacTuzX-dot/LeoPackz/releases/download/v1/LeoPacks.zip" 
